dca8c33387efcb1ef17627c2536b8425c32cbc96
leo carvalho <lndrcrvlh@gmail.com>
2022-06-14 16:37:51 -0400

    displays count from API on page load

--- src/components/count.jsx
+++ src/components/count.jsx
@@ -1,9 +1,15 @@
-import React from 'react'
+import { React, useState, useEffect } from "react";
 
 function Count() {
-  return (
-    <div>count</div>
-  )
+  const [count, setCount] = useState([]);
+
+  useEffect(() => {
+    fetch(
+      "https://api.countapi.xyz/hit/namespace/1ccb732e-b55a-4404-ad3f-0f99c02fe44e"
+    ).then((resp) => resp.json()).then(data => setCount(data.value))
+  }, []);
+
+  return <div>{count}</div>;
 }
 
-export default Count
+export default Count;
