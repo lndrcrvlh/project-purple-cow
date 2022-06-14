1b438aace085ea80ea3a54a136863334a44e8038
leo carvalho <lndrcrvlh@gmail.com>
2022-06-14 17:30:45 -0400

    Adds button that displays count from API on click

--- src/components/count.jsx
+++ src/components/count.jsx
@@ -2,14 +2,25 @@ import { React, useState, useEffect } from "react";
 
 function Count() {
   const [count, setCount] = useState([]);
+  const [checkCount, setCheckCount] = useState(false);
 
   useEffect(() => {
-    fetch(
-      "https://api.countapi.xyz/hit/namespace/1ccb732e-b55a-4404-ad3f-0f99c02fe44e"
-    ).then((resp) => resp.json()).then(data => setCount(data.value))
-  }, []);
+    if (checkCount=== true) {setCount(() =>
+          fetch(
+            "https://api.countapi.xyz/hit/namespace/1ccb732e-b55a-4404-ad3f-0f99c02fe44e"
+          )
+            .then((resp) => resp.json())
+            .then((data) => setCount(data))
+        )
+        setCheckCount(false)}
+  }, [ checkCount]);
 
-  return <div>{count}</div>;
+  return (
+    <div>
+      {count && <p>{count.value}</p>}
+      <button onClick={() => setCheckCount(true)}>get the count</button>
+    </div>
+  );
 }
 
 export default Count;
